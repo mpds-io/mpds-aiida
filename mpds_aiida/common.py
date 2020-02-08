66708ea63a349530df5a2ae0d00100231a3cf797
Evgeny Blokhin <eb@tilde.pro>
2020-02-08 02:08:29 +0100

    Add more utility code to be re-used

---
+++ mpds_aiida/common.py
@@ -0,0 +1,104 @@
+
+import os
+from collections import namedtuple
+
+import yaml
+from ase.data import chemical_symbols
+
+from aiida_crystal.io.d12_write import write_input
+from aiida_crystal.io.basis import BasisFile # NB only used to determine ecp
+from mpds_client import APIError
+
+from mpds_aiida import CALC_TPL_DIR
+
+
+verbatim_basis = namedtuple("basis", field_names="content, all_electron")
+
+def get_basis_sets(repo_dir):
+    """
+    Keeps all available BS in a dict for convenience
+    NB. we assume BS repo_dir = AiiDA's *basis_family*
+    """
+    assert os.path.exists(repo_dir)
+    bs_repo = {}
+    for filename in os.listdir(repo_dir):
+        if not filename.endswith('.basis'):
+            continue
+
+        el = filename.split('.')[0]
+        assert el in chemical_symbols
+        with open(repo_dir + os.sep + filename, 'r') as f:
+            bs_str = f.read().strip()
+
+        bs_parsed = BasisFile().parse(bs_str)
+        bs_repo[el] = verbatim_basis(content=bs_str, all_electron=('ecp' not in bs_parsed))
+
+    return bs_repo
+
+
+def get_template(template='minimal.yml'):
+    """
+    Templates present the permanent calc setup
+    """
+    with open(os.path.join(CALC_TPL_DIR, template)) as f:
+        calc = yaml.load(f.read())
+    assert 'parameters' in calc and 'crystal' in calc['parameters'] and 'basis_family' in calc
+    return calc
+
+
+def get_input(calc_params_crystal, elements, bs_repo, label):
+    """
+    Generates a test input
+    """
+    calc_params_crystal['title'] = label
+    return write_input(calc_params_crystal, [bs_repo[el] for el in elements])
+
+
+supported_arities = {1: 'unary', 2: 'binary', 3: 'ternary', 4: 'quaternary', 5: 'quinary'}
+
+def get_structures(mpds_api, elements, more_query_args=None):
+    """
+    Given some arbitrary chemical elements,
+    get their possible crystalline structures
+    """
+    assert sorted(list(set(elements))) == sorted(elements) and \
+    len(elements) <= len(supported_arities)
+
+    structures = []
+    query = {
+        "props": "atomic structure",
+        "elements": '-'.join(elements),
+        "classes": supported_arities[len(elements)] + ", non-disordered"
+    }
+    if more_query_args and type(more_query_args) == dict:
+        query.update(more_query_args)
+
+    try:
+        for item in mpds_api.get_data(
+            query,
+            fields={'S': [
+                'phase',
+                'occs_noneq', # non-disordered phases may have != 1
+                'cell_abc',
+                'sg_n',
+                'basis_noneq',
+                'els_noneq'
+            ]}
+        ):
+            if any([occ != 1 for occ in item[1]]):
+                continue
+
+            ase_obj = mpds_api.compile_crystal(item, flavor='ase')
+            if not ase_obj:
+                continue
+
+            ase_obj.info['phase'] = item[0]
+            structures.append(ase_obj)
+
+    except APIError as ex:
+        if ex.code == 204:
+            print("No results!")
+            return []
+        else: raise
+
+    return structures
