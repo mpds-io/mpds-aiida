79750af29415567217f9a01d0ee6ead705bcc38a
Evgeny Blokhin <eb@tilde.pro>
2020-02-08 02:12:42 +0100

    Bring back *yascheduler* examples and func tests into the repo

---
+++ bin/ya_submit_cif.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# NB. pip install git+https://github.com/tilde-lab/yascheduler git+https://github.com/mpds-io/mpds-ml-labs
+
+import os
+import sys
+from configparser import ConfigParser
+
+from aiida_crystal.io.f34 import Fort34
+from mpds_ml_labs.struct_utils import detect_format, refine
+from mpds_ml_labs.cif_utils import cif_to_ase
+
+from yascheduler import CONFIG_FILE
+from yascheduler.scheduler import Yascheduler
+
+import spglib
+from mpds_aiida.common import get_template, get_basis_sets, get_input
+
+
+structure = open(sys.argv[1]).read()
+assert detect_format(structure) == 'cif'
+ase_obj, error = cif_to_ase(structure)
+assert not error, error
+
+try: symprec = float(sys.argv[2])
+except: symprec = 3E-02 # NB needs tuning
+print('symprec = %s' % symprec)
+
+label = sys.argv[1].split(os.sep)[-1].split('.')[0] + \
+    " " + spglib.get_spacegroup(ase_obj, symprec=symprec)
+
+ase_obj, error = refine(ase_obj, accuracy=symprec, conventional_cell=True)
+assert not error, error
+
+yaconfig = ConfigParser()
+yaconfig.read(CONFIG_FILE)
+yac = Yascheduler(yaconfig)
+
+calc_setup = get_template()
+bs_repo = get_basis_sets(calc_setup['basis_family'])
+
+#bs_repo_tzvp = get_basis_sets('./tzvp_RE')
+#bs_repo_other = get_basis_sets('./hand_made_bs')
+#bs_repo = {**bs_repo_tzvp , **bs_repo_other}
+
+elements = list(set(ase_obj.get_chemical_symbols()))
+f34_input = Fort34([bs_repo[el] for el in elements])
+struct_input = str(f34_input.from_ase(ase_obj))
+setup_input = get_input(calc_setup['parameters']['crystal'], elements, bs_repo, label)
+
+result = yac.queue_submit_task(label, dict(structure=struct_input, input=setup_input))
+print(label)
+print(result)
---
+++ bin/ya_submit_ht_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# NB. pip install git+https://github.com/tilde-lab/yascheduler
+
+from itertools import product
+from configparser import ConfigParser
+
+import numpy as np
+from aiida_crystal.io.f34 import Fort34
+
+from yascheduler import CONFIG_FILE
+from yascheduler.scheduler import Yascheduler
+
+from mpds_client import MPDSDataRetrieval
+from mpds_aiida.common import get_template, get_basis_sets, get_structures, get_input
+
+
+ela = ['Li', 'Na', 'K', 'Rb', 'Be', 'Mg', 'Ca', 'Sr']
+elb = ['F', 'Cl', 'Br', 'I', 'O', 'S', 'Se', 'Te']
+
+config = ConfigParser()
+config.read(CONFIG_FILE)
+yac = Yascheduler(config)
+
+client = MPDSDataRetrieval()
+
+calc_setup = get_template()
+bs_repo = get_basis_sets(calc_setup['basis_family'])
+
+for elem_pair in product(ela, elb):
+    print(elem_pair)
+    structures = get_structures(client, elem_pair, more_query_args=dict(lattices='cubic'))
+    structures_by_sgn = {}
+
+    for s in structures:
+        structures_by_sgn.setdefault(s.info['spacegroup'].no, []).append(s)
+
+    for sgn_cls in structures_by_sgn:
+        # get structures with the minimal number of atoms and find one with the median cell vectors
+        minimal_struct = min([len(s) for s in structures_by_sgn[sgn_cls]])
+        cells = np.array([s.get_cell().reshape(9) for s in structures_by_sgn[sgn_cls] if len(s) == minimal_struct])
+        median_cell = np.median(cells, axis=0)
+        median_idx = int(np.argmin(np.sum((cells - median_cell)**2, axis=1)**0.5))
+        target_obj = structures_by_sgn[sgn_cls][median_idx]
+
+        f34_input = Fort34([bs_repo[el] for el in elem_pair])
+        struct_input = f34_input.from_ase(target_obj)
+        struct_input = str(struct_input)
+        setup_input = get_input(calc_setup['parameters']['crystal'], elem_pair, bs_repo, target_obj.info['phase'])
+
+        yac.queue_submit_task(target_obj.info['phase'], dict(structure=struct_input, input=setup_input))
