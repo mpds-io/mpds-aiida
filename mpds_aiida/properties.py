aef11e91217a01288cf3a2c62aca41aed328430f
Evgeny Blokhin <eb@tilde.pro>
2020-09-24 14:22:16 +0200

    Refactor and bugfix the electron props generation

--- mpds_aiida/properties.py
+++ mpds_aiida/properties.py
@@ -11,6 +11,7 @@ import random
 import shutil
 import subprocess
 from configparser import ConfigParser
+import warnings
 import string
 from datetime import datetime
 
@@ -99,14 +100,26 @@ def kill(proc_pid):
     process.kill()
 
 
-def run_properties_direct(wf_path, input_dict, work_folder=None):
+def get_avg_charges(ase_obj):
+    at_type_chgs = {}
+    for atom in ase_obj:
+        at_type_chgs.setdefault(atom.symbol, []).append(atom.charge)
+
+    if sum([sum(at_type_chgs[at_type]) for at_type in at_type_chgs]) == 0.0:
+        return None
+
+    return {at_type: np.average(at_type_chgs[at_type]) for at_type in at_type_chgs}
+
+
+def properties_run_direct(wf_path, input_dict, work_folder=None):
     """
     This procedure runs properties
-    outside of AiiDA graph and scheduler
+    outside of the AiiDA graph and scheduler,
+    returns (bands, dos), work_folder, error
     """
-    assert wf_path.endswith('fort.9') and 'band' in input_dict and 'dos' in input_dict \
-        and 'first' not in input_dict['dos'] and 'first' not in input_dict['band'] \
-        and 'last' not in input_dict['dos'] and 'last' not in input_dict['band']
+    assert wf_path.endswith('fort.9') and 'band' in input_dict and 'dos' in input_dict
+    assert 'first' not in input_dict['dos'] and 'first' not in input_dict['band']
+    assert 'last' not in input_dict['dos'] and 'last' not in input_dict['band']
 
     if not work_folder:
         work_folder = os.path.join(config.get('local', 'data_dir'), '_'.join([
@@ -120,15 +133,15 @@ def run_properties_direct(wf_path, input_dict, work_folder=None):
     shutil.copy(os.path.join(os.path.dirname(wf_path), 'fort.34'), work_folder) # save structure
 
     wf = Fort9(os.path.join(work_folder, 'fort.9'))
-
     # automatic generation of k-point path
     #structure = wf.get_structure()
+    last_state = wf.get_ao_number()
     # NB fort.9 may produce slightly different structure, so use fort.34
+
     f34 = f34_input.read(os.path.join(os.path.dirname(wf_path), 'fort.34'))
     structure = f34.to_aiida()
 
     shrink, _, kpath = get_shrink_kpoints_path(structure)
-    last_state = wf.get_ao_number()
     input_dict['band']['shrink'] = shrink
     input_dict['band']['bands'] = kpath
 
@@ -152,86 +165,91 @@ def run_properties_direct(wf_path, input_dict, work_folder=None):
         p.wait(timeout=EXEC_TIMEOUT)
     except subprocess.TimeoutExpired:
         kill(p.pid)
-        print("Killed as too time-consuming")
+        return None, work_folder, 'PROPERTIES killed as too time-consuming'
 
     print("Done in %1.2f sc" % (time.time() - start_time))
 
     if p.returncode != 0:
-        return None, 'PROPERTIES failed, see %s' % work_folder
+        return None, work_folder, 'PROPERTIES failed'
 
     if not os.path.exists(os.path.join(work_folder, 'BAND.DAT')) \
         or not os.path.exists(os.path.join(work_folder, 'DOSS.DAT')) \
         or not os.path.exists(os.path.join(work_folder, 'fort.25')):
-        return None, 'No PROPERTIES outputs at %s' % work_folder
+        return None, work_folder, 'PROPERTIES missing outputs'
 
-    edata = Fort25(os.path.join(work_folder, 'fort.25')).parse()
+    result = Fort25(os.path.join(work_folder, 'fort.25')).parse()
+    bands = result.get("BAND", None)
+    dos = result.get("DOSS", None)
+    if not bands or not dos:
+        return None, work_folder, 'PROPERTIES missing BANDS or DOS'
 
-    assert sum(edata['DOSS']['e']) == 0 # all zeros in fort.25
-    assert len(edata['DOSS']['dos_up'][0]) == len(edata['DOSS']['dos_down'][0])
-    print('>N DOSS: %s' % len(edata['DOSS']['dos_up'][0]))
+    # get rid of the negative DOS artifacts
+    dos['dos_up'][ dos['dos_up'] < 0 ] = 0
+    dos['dos_up'] *= Hartree
+    if dos['dos_down'] is not None:
+        assert len(dos['dos_up'][0]) == len(dos['dos_down'][0])
+        dos['dos_down'][ dos['dos_down'] < 0 ] = 0
+        dos['dos_down'] *= Hartree
+        # sum up and down: FIXME
+        dos['dos_up'] += dos['dos_down']
 
-    path = edata['BAND']['path']
-    k_number = edata['BAND']['n_k']
+    dos['e'] *= Hartree
+    dos['e_fermi'] *= Hartree
 
     #cell = wf.get_cell(scale=True) # for path construction we're getting geometry from fort.9
     # NB fort.9 may produce slightly different structure, so use fort.34
     cell = f34.abc, f34.positions, f34.atomic_numbers
 
-    path_description = construct_kpoints_path(cell, path, shrink, k_number)
+    path_description = construct_kpoints_path(cell, bands['path'], shrink, bands['n_k'])
     # find k-points along the path
     k_points = get_explicit_kpoints_path(structure, path_description)['explicit_kpoints']
 
-    # pass through the internal AiiDA repr, FIXME?
+    # pass through the internal AiiDA repr
     bands_data = DataFactory('array.bands')()
     bands_data.set_kpointsdata(k_points)
-    bands_data.set_bands(edata['BAND']['bands'])
+    if bands['bands_down'] is not None:
+        # sum up and down: FIXME
+        bands_data.set_bands(np.hstack(( (bands['bands_up'] - bands['e_fermi']) * Hartree, (bands['bands_down'] - bands['e_fermi']) * Hartree )))
+    else:
+        bands_data.set_bands((bands['bands_up'] - bands['e_fermi']) * Hartree)
 
-    edata['BAND']['bands'] = bands_data.get_bands()
-    k_points = bands_data.get_kpoints().tolist()
-    print('>N KPOINTS: %s' % len(k_points))
-    print('>N BANDS: %s' % len(edata['BAND']['bands'][0]))
-    print('>E_Fermi in bands: %s' % edata['BAND']['e_fermi'])
-    print('>E_Fermi in   dos: %s' % edata['DOSS']['e_fermi'])
+    return (bands_data, dos), work_folder, None
 
-    edata['BAND']['bands'] -= edata['BAND']['e_fermi']
-    edata['BAND']['bands'] *= Hartree
 
-    edata['DOSS']['dos_up'] += edata['DOSS']['dos_down']
-    edata['DOSS']['dos_up'] *= Hartree
+def properties_export(bands_data, dos_data, ase_struct):
 
-    # get rid of the negative DOS artifacts
-    edata['DOSS']['dos_up'][ edata['DOSS']['dos_up'] < 0 ] = 0
-
-    e_min, e_max = np.amin(edata['BAND']['bands']), np.amax(edata['BAND']['bands'])
-    dos_energies = np.linspace(e_min, e_max, num=len(edata['DOSS']['dos_up'][0]))
-    stripes = edata['BAND']['bands'].transpose().copy()
+    bands_array = bands_data.get_array('bands')
+    e_min, e_max = np.amin(bands_array), np.amax(bands_array)
+    dos_energies = np.linspace(e_min, e_max, num=len(dos_data['dos_up'][0]))
+    stripes = bands_array.transpose().copy()
 
     if is_conductor(stripes):
         indirect_gap, direct_gap = None, None
     else:
         indirect_gap, direct_gap = get_band_gap_info(stripes)
 
-    # save only the range of the interest
-    E_MIN, E_MAX = -15, 20
+    if guess_metal(ase_struct) and (direct_gap or indirect_gap):
+        warnings.warn('%s: SUSPICION FOR METAL WITH BAND GAP' % ase_struct.get_chemical_formula())
+
+    # export only the range of the interest
+    E_MIN, E_MAX = -10, 20
     stripes = stripes[ (stripes[:,0] > E_MIN) & (stripes[:,0] < E_MAX) ]
     dos = []
-    for n, i in enumerate(dos_energies): # FIXME use numpy advanced slicing
-        if E_MIN < i < E_MAX:
-            dos.append(edata['DOSS']['dos_up'][0][n])
+    for n, ene in enumerate(dos_energies): # FIXME use numpy advanced slicing
+        if E_MIN < ene < E_MAX:
+            dos.append(dos_data['dos_up'][0][n])
     dos_energies = dos_energies[ (dos_energies > E_MIN) & (dos_energies < E_MAX) ]
 
     return {
-        # - gaps
+        # gaps
         'direct_gap': direct_gap,
         'indirect_gap': indirect_gap,
-        # - dos values
+
+        # dos values
         'dos': np.round(np.array(dos), 3).tolist(),
         'levels': np.round(dos_energies, 3).tolist(),
-        # - bands values
-        'k_points': k_points,
+
+        # bands values
+        'k_points': bands_data.get_array('kpoints').tolist(),
         'stripes': np.round(stripes, 3).tolist(),
-        # - auxiliary
-        'work_folder': work_folder,
-        'units': 'eV',
-        'error': None
-    }, None
+    }
--- scripts/run_properties.py
+++ scripts/run_properties.py
@@ -1,30 +1,66 @@
 #!/usr/bin/env python3
 
-import sys
+import os, sys
 import json
 #from pprint import pprint
 
-from mpds_aiida.common import get_template
-from mpds_aiida.properties import run_properties_direct
+import numpy as np
+import matplotlib.pyplot as plt
+
 from aiida import load_profile
+from aiida_crystal_dft.io.f9 import Fort9
+from mpds_aiida.common import get_template
+from mpds_aiida.properties import properties_run_direct, properties_export
+
 
 load_profile()
 calc_setup = get_template('production.yml')
 
-result, error = run_properties_direct(sys.argv[1], calc_setup['parameters']['properties'])
+folder = '/tmp/_props'
+if os.path.exists(folder):
+    assert not os.listdir(folder)
+else:
+    os.makedirs(folder)
+
+output, _, error = properties_run_direct(sys.argv[1], calc_setup['parameters']['properties'], folder)
 if error:
     raise RuntimeError(error)
 
-#print(result)
+bands, dos = output
+
 print("Writing JSONs")
 
-bands = {'bands': result['stripes'], 'kpoints': result['k_points']}
-dos = {'dos': result['dos'], 'levels': result['levels']}
+wf = Fort9(sys.argv[1])
+result = properties_export(bands, dos, wf.get_ase())
 
 f = open('bands.json', 'w')
-f.write(json.dumps({'sample': {'measurement': [{'property': {'matrix': bands}}]}, 'use_visavis_type': 'eigenplot'}))
+f.write(json.dumps({'sample': {'measurement': [{'property': {'matrix': {
+    'bands': result['stripes'],
+    'kpoints': result['k_points']
+}}}]}, 'use_visavis_type': 'eigenplot'}))
 f.close()
 
 f = open('dos.json', 'w')
-f.write(json.dumps({'sample': {'measurement': [{'property': {'matrix': dos}}]}, 'use_visavis_type': 'eigenplot'}))
+f.write(json.dumps({'sample': {'measurement': [{'property': {'matrix': {
+    'dos': result['dos'],
+    'levels': result['levels']
+}}}]}, 'use_visavis_type': 'eigenplot'}))
 f.close()
+
+print("Writing e_props.png")
+
+f, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [3, 1]}, figsize=(10, 8))
+x = np.arange(bands.get_array('kpoints').shape[0])
+ax0.set_xlim(0, len(x)-1)
+ax0.set_ylim(-10, 20)
+
+ax0.set_xticks(bands.attributes['label_numbers'])
+ax0.set_xticklabels(bands.attributes['labels'])
+ax0.plot(x, bands.get_array('bands'), 'b')
+ax1.set_xlim(0, 2000)
+ax1.set_ylim(-10, 20)
+ax1.plot(dos['dos_up'].T, (dos['e'] - dos['e_fermi']))
+
+plt.savefig('e_props.png', dpi=250)
+
+print('E_dir, E_ind = ', result['direct_gap'], result['indirect_gap'])
