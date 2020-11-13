79586da4c27c4bf442557c07e2fd424c6c555bd8
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2020-11-14 00:33:33 +0500

    Workflow entry points redesign

--- mpds_aiida/workflows/cif.py
+++ mpds_aiida/workflows/cif.py
@@ -2,4 +2,31 @@
 """
 The MPDS workflow using structure from CIF file
 """
+from aiida_crystal_dft.utils import get_data_class
+from .crystal import MPDSCrystalWorkChain
+from mpds_ml_labs.struct_utils import detect_format
+from mpds_ml_labs.cif_utils import cif_to_ase
 
+
+class CIFStructureWorkChain(MPDSCrystalWorkChain):
+
+    @classmethod
+    def define(cls, spec):
+        super(CIFStructureWorkChain, cls).define(spec)
+        # one required input: CIF input file name
+        spec.input('structure', valid_type=get_data_class('str'), required=True)
+        # 59X - CIF related errors
+        spec.exit_code(590, 'ERROR_DISORDERED_STRUCTURE', message='Structure is disordered')
+        spec.exit_code(591, 'ERROR_PARSING_CIF', message='Error in getting ASE object form CIF file')
+
+    def get_geometry(self):
+        """ Getting geometry from MPDS database
+        """
+        structure = open(self.inputs.structure).read()
+        assert detect_format(structure) == 'cif'
+        ase_obj, error = cif_to_ase(structure)
+        if error:
+            return self.exit_codes.ERROR_PARSING_CIF
+        if 'disordered' in ase_obj.info:
+            return self.exit_codes.ERROR_DISORDERED_STRUCTURE
+        return get_data_class('structure')(ase=ase_obj)
--- mpds_aiida/workflows/mpds.py
+++ mpds_aiida/workflows/mpds.py
@@ -3,6 +3,7 @@
 """
 The MPDS workflow for AiiDA that gets structure with MPDS query
 """
+import os
 import time
 import random
 import numpy as np
@@ -19,13 +20,21 @@ class MPDSStructureWorkChain(MPDSCrystalWorkChain):
         super(MPDSStructureWorkChain, cls).define(spec)
         # one required input: MPDS phase id
         spec.input('mpds_query', valid_type=get_data_class('dict'), required=True)
+        # 50X - errors related to MPDS retrieval
+        spec.exit_code(501, 'ERROR_NO_MPDS_API_KEY', message='MPDS API key not set')
+        spec.exit_code(502, 'ERROR_API_ERROR', message='MPDS API Error')
+        spec.exit_code(503, 'ERROR_NO_HITS', message='Request returned nothing')
 
     def get_geometry(self):
         """ Getting geometry from MPDS database
         """
-        client = MPDSDataRetrieval()
-        query_dict = self.inputs.mpds_query.get_dict()
+        # check for API key
+        api_key = os.getenv('MPDS_KEY')
+        if not api_key:
+            return self.exit_codes.ERROR_NO_MPDS_API_KEY
+        client = MPDSDataRetrieval(api_key=api_key)
 
+        query_dict = self.inputs.mpds_query.get_dict()
         # prepare query
         query_dict['props'] = 'atomic structure'
         if 'classes' in query_dict:
@@ -49,12 +58,14 @@ class MPDSStructureWorkChain(MPDSCrystalWorkChain):
                 time.sleep(random.choice([2 * 2**m for m in range(5)]))
                 return self.get_geometry()
             else:
-                raise
+                self.report(f'MPDS API error: {str(ex)}')
+                self.logger.error(f'MPDS API error: {str(ex)}')
+                return self.exit_codes.ERROR_API_ERROR
 
         structs = [client.compile_crystal(line, flavor='ase') for line in answer]
         structs = list(filter(None, structs))
         if not structs:
-            raise APIError('No crystal structures returned')
+            return self.exit_codes.ERROR_NO_HITS
         minimal_struct = min([len(s) for s in structs])
 
         # get structures with minimal number of atoms and find the one with median cell vectors
